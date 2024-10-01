a0692bfcc165527dcd43283977d2c75063a53eff
asif1122se <35570584+asif1122se@users.noreply.github.com>
2024-10-02 03:01:54 +0500

    Update page.js

--- src/app/[locale]/services/page.js
+++ src/app/[locale]/services/page.js
@@ -9,7 +9,8 @@ const page = () => {
   const cardData = [
     {
       id: 1,
-      imageUrl: "/images/digital-transformation.png",
+      // imageUrl: "/images/digital-transformation.png",
+      imageUrl: "/images/custom-software.png",
       heading: "AI-Powered Chat Super Agents",
       text: "Our AI-Powered Chat Super Agents don’t just respond to customer inquiries—they also automate workflows that extend beyond the chat interface. From managing customer support to pulling relevant data and automating follow-ups, these agents are designed to enhance both customer experience and operational efficiency."  },
     {
